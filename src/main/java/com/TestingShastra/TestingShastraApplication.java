c888b5f3218e71db40081313e7b828021b767c18
Amit <amitrajmane7891@gmail.com>
2022-07-16 13:51:50 +0530

    Basic structure of the appllication

---
+++ src/main/java/com/TestingShastra/TestingShastraApplication.java
@@ -0,0 +1,13 @@
+package com.TestingShastra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingShastraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingShastraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TestingShastra/TestingShastraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestingShastra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingShastraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
